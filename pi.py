79319a6bf2e557345a5f0d9e607ae2e70442571c
Łukasz Kasprzyk <81863998+Diakoptes@users.noreply.github.com>
2022-03-07 15:52:08 +0100

    Create pi.py

---
+++ pi.py
@@ -0,0 +1,13 @@
+#Find PI to the Nth Digit - Enter a number and have the program generate π (pi) up to that many decimal places. 
+#Keep a limit to how far the program will go.
+
+import math
+
+def round_pi():
+    
+    n = int(input("number of playces: "))
+    
+    if n<100:
+        return round(math.pi,n)
+    else:
+        print("number is to big, plese get lower" + round_pi())
