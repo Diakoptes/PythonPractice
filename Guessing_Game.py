57486d93bb4494d72ee30168d7bf56743711288c
Łukasz Kasprzyk <81863998+Diakoptes@users.noreply.github.com>
2022-03-08 10:58:18 +0100

    Create Guessing_Game.py

---
+++ Guessing_Game.py
@@ -0,0 +1,31 @@
+import random 
+
+def guessing_game():
+    
+    
+    guess = random.randrange(1, 100)
+    playing = True
+    posibilities = list(range(0,101))
+    start = "I chose a numeber.\nIt is in range 1-100.\nGuess or die!"
+    
+    print(start)
+    
+    while playing:
+        
+        your_guess = input("What number has been chosen: ")
+        
+        if your_guess == "exit":
+            playing = False
+        
+        elif your_guess.isdigit() == False or int(your_guess) not in posibilities:
+            print("Chose number 1-100, or exit to close.")
+        
+        elif int(your_guess) < guess:
+            print("Too low...")
+            
+        elif int(your_guess) > guess:
+            print("Too high...")
+            
+        elif int(your_guess) == guess:
+            print("Just right!")
+            playing = False
